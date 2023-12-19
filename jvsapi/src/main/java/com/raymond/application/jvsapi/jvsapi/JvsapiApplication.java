7704d903542034b7b5613db5aabd951add39b06d
Raymond <r.haensch@hotmail.com>
2023-12-19 19:50:49 +0100

    Init with spring, maven

---
+++ jvsapi/src/main/java/com/raymond/application/jvsapi/jvsapi/JvsapiApplication.java
@@ -0,0 +1,13 @@
+package com.raymond.application.jvsapi.jvsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JvsapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JvsapiApplication.class, args);
+	}
+
+}
